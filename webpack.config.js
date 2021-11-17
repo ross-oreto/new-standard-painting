1f81d8db9201a6b36851750001c8ee03cfef05c7
ross-oreto <ross.oreto@pm.me>
2021-11-17 13:39:01 -0600

    `...` syntax to extend existing minimizers

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,8 @@ module.exports = {
     optimization: {
         minimizer: [
             // For webpack@5 you can use the `...` syntax to extend existing minimizers
-            // (i.e. `terser-webpack-plugin`), uncomment the next line
-            // `...`,
+            // (i.e. `terser-webpack-plugin`)
+            `...`,
             new CssMinimizerPlugin(),
         ],
     },
