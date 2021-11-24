3ca192834cd5d75eb8fc92f27915e4dfa3e77b71
ross-oreto <ross.oreto@pm.me>
2021-11-23 22:09:17 -0600

    Add form validation. Add openroute api.

--- src/Controllers/App.php
+++ src/Controllers/App.php
@@ -2,6 +2,8 @@
 
 namespace Alex\Nsp\Controllers;
 
+use Base;
+use Oreto\F3Willow\Psr7\Psr7;
 use Oreto\F3Willow\Routing\Routes;
 use Oreto\F3Willow\Willow;
 
@@ -10,10 +12,41 @@ class App extends Willow {
         return Routes::create(self::class)
             ->GET("home", "/")->handler('index')
             ->GET("schedule", "/schedule")->handler('schedule')
+            ->GET("address", "/address/@address")->handler('address')->ajax()
             ->build();
     }
 
-    function schedule($f3) {
+    function schedule(Base $f3) {
         echo $this->render("schedule");
+    }
+
+    function address(Base $f3) {
+        $address = urlencode($f3->get("PARAMS.address"));
+        $curl = curl_init();
+        $apiUrl = "https://api.openrouteservice.org/geocode/search";
+        $region = "whosonfirst:region:85688701";
+
+        $apiKey = $f3->get("openroute.apikey");
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "$apiUrl?api_key=$apiKey&text=$address&boundary.gid=$region&layers=address&size=3",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+        $response = curl_exec($curl);
+        $json = json_decode($response);
+
+        if (sizeof($json->{'features'}) == 1) {
+            header("Content-Type: ".Psr7::APPLICATION_JSON, true, 200);
+            echo json_encode($json->{'features'}[0]);
+        } else {
+            header("Content-Type: text/plain", true, 400);
+            echo Willow::dict("schedule.address.not.found");
+        }
+        curl_close($curl);
     }
 }
--- webapp/assets/app.js
+++ webapp/assets/app.js
@@ -1,4 +1,5 @@
-import {$} from "./jquery.js"
+import {$} from "./jquery.js";
+import "./jquery.validate.js";
 
 $(function () {
     $(".navbar-burger").on('click',function() {
@@ -6,4 +7,79 @@ $(function () {
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
     });
+
+    $("#estimate-button").click(() => {
+        const rules = {
+            'name': {
+                required: true
+            }
+            , 'address': {
+                required: true
+                , equalTo: "input[name='suggestedAddress']"
+            }
+            , 'email': {
+                required: true,
+                email: true
+            }
+            , 'phone': {
+                phoneUS: true,
+                required: {
+                    depends: () => $("input[name=phone]").is(":checked")
+                }
+            }
+        };
+        const highlight = function(element, errorClass) {
+            const el = $(element);
+            el.fadeOut(function() {
+                el.fadeIn();
+                el.addClass("is-danger");
+                el.siblings('label').addClass('has-text-danger');
+            });
+        }
+        const unhighlight = function(element, errorClass, validClass) {
+            $(element).removeClass("is-danger");
+        }
+        const options = {
+            onkeyup: false
+            , onfocusout: false
+            , rules: rules
+            , highlight: highlight
+            , unhighlight: unhighlight
+            , messages: {
+                'address': { equalTo: 'Address not found' }
+            }
+        };
+
+        const inputAddress = $("input[name='address']");
+        const suggestedAddress = $("input[name='suggestedAddress']");
+        if (validateForm(options)) {
+            requestEstimate();
+        } else if(inputAddress.val()) {
+            $.get(`/address/${inputAddress.val()}`, (data) => {
+                $("input[name='geocode']").val(data.geometry.coordinates.join(','));
+                const p = data.properties;
+                const address = `${p.name}, ${p.locality}, ${p.region_a}, ${p.postalcode}`;
+                suggestedAddress.val(address);
+                inputAddress.val(address);
+
+                if(validateForm(options)) {
+                    requestEstimate();
+                }
+            }).fail((data) => {
+                suggestedAddress.val('');
+                options.messages = { 'address': { equalTo: `${data.responseText}` }}
+                validateForm(options);
+            });
+        }
+    });
+
+    function validateForm(options) {
+        const form = $("#schedule");
+        form.validate(options);
+        return form.valid();
+    }
+
+    function requestEstimate() {
+
+    }
 });
