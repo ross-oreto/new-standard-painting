6cdd7eb406df8c1083e2d8a89814d5d305e525db
ross-oreto <ross.oreto@pm.me>
2021-12-01 18:54:22 -0600

    fix jquery reference

--- webapp/assets/jquery.validate.js
+++ webapp/assets/jquery.validate.js
@@ -8,9 +8,9 @@
  */
 (function( factory ) {
     if ( typeof define === "function" && define.amd ) {
-        define( ["jquery"], factory );
+        define( ["./jquery"], factory );
     } else if (typeof module === "object" && module.exports) {
-        module.exports = factory( require( "jquery" ) );
+        module.exports = factory( require( "./jquery" ) );
     } else {
         factory( jQuery );
     }
